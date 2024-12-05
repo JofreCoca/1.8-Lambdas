17a99d12078faa231845ad97b0e7053b02727064
JofreCoca <jofrecoca@gmail.com>
2024-12-05 08:21:14 +0100

    Finish l1 e8

--- src/Level_1_Exercise_8/Main.java
+++ src/Level_1_Exercise_8/Main.java
@@ -1,26 +1,13 @@
 package Level_1_Exercise_8;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
-        List<Object> elements = new ArrayList<>();
-        elements.add("Hello");
-        elements.add("goodbye");
-        elements.add(3.141516);
-        elements.add(23);
-        elements.add("One");
-        elements.add("Two");
+        ReverseString reverseFunction = input -> new StringBuilder(input).reverse().toString();
 
-        List<String> SortedStrings = elements.stream()
-                .filter(element -> element instanceof String)
-                .map(element -> (String) element)
-                .sorted(Comparator.comparingInt(String::length).reversed())
-                .collect(Collectors.toList());
+        String stringOriginal = "Hello world";
+        String stringReverse = reverseFunction.reverse(stringOriginal);
 
-        System.out.println("Chains ordered by length: " + SortedStrings);
+        System.out.println("string Original: " + stringOriginal);
+        System.out.println("String Reverse: " + stringReverse);
     }
 }
---
+++ src/Level_1_Exercise_8/ReverseString.java
@@ -0,0 +1,6 @@
+package Level_1_Exercise_8;
+
+@FunctionalInterface
+interface ReverseString {
+    String reverse(String input);
+}
