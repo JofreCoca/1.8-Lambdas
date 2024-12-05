e3ca44a3139a5f4a0d1a75565f124001403153bb
JofreCoca <jofrecoca@gmail.com>
2024-12-05 07:53:46 +0100

    Finish l1 e5

--- src/Level_1_Exercise_5/Main.java
+++ src/Level_1_Exercise_5/Main.java
@@ -3,7 +3,6 @@ package Level_1_Exercise_5;
 public class Main {
     public static void main(String[] args) {
         PiProvider piProvider = () -> 3.1415;
-
         System.out.println("The PI value is: " + piProvider.getPiValue());
     }
 }
