4bd2ec7f6c048e0eff846b2949f4758c076a2259
JofreCoca <jofrecoca@gmail.com>
2024-12-05 07:37:30 +0100

    Finish l1 e2

--- src/Level_1_Exercise_2/Main.java
+++ src/Level_1_Exercise_2/Main.java
@@ -5,14 +5,14 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> list = List.of("one","two","three","four","five","six");
+        List<String> list = List.of("one","two","three","four","five","six","forty nine");
         List<String> stringsContainingTheLetterO = getStringsWithO(list);
 
         System.out.println("Result: " + stringsContainingTheLetterO);
     }
     public static List<String> getStringsWithO(List<String> stringsContainingTheLetterO) {
         return stringsContainingTheLetterO.stream()
-                .filter(chain -> chain.toLowerCase().contains("o"))
+                .filter(chain -> chain.toLowerCase().contains("o") && chain.length()>5)
                 .collect(Collectors.toList());
     }
 }
